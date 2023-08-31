4db6ef665ced25a7e3cda0653819f4b5a29bbc19
rafacnelli <65323881+rafaelnel@users.noreply.github.com>
2023-08-31 15:58:13 -0300

    .

--- script.js
+++ script.js
@@ -6,3 +6,21 @@ function Navbar_slide() {
       h.style.display = "flex";
     }
   }
+
+  function Sanfona() {
+    let sb = document.getElementById("qs_div");
+    let sanf = document.getElementById("sanf");
+    if (sb.style.display === "flex") {
+      sb.style.display = "none";
+    } else {
+      sb.style.display = "flex";
+    }
+    if (sb.style.maxHeight) {
+      sb.style.maxHeight = null;
+    } else {
+      sb.style.maxHeight = sb.scrollHeight + "px";
+    } 
+    sanf.classList.toggle("ativo");
+    
+  }
+
